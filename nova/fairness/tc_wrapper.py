cef75eb8b6f27e0b9c57ea1bf07a00ffdc2ac36c
Simon <simon.mueller@simondo.ch>
2016-04-18 17:50:37 +0200

    code cleanup

--- nova/fairness/tc_wrapper.py
+++ nova/fairness/tc_wrapper.py
@@ -2,9 +2,12 @@ from nova import utils
 from nova.openstack.common import log as logging
 from nova.openstack.common import processutils
 
-"""Setting up tc with HFSC for proportional sharing
+# TODO interface is hardcoded in _find_bridge_interface() for now, as resource_allocation method does not account for the specific configuration in use
+def hfsc_proportional_share(interface, prios, upper_limit):
+    """
+    Sets up the HFSC qdisc with the specified priorities for proportional sharing
 
-    Example for three hosts:
+    Example tc commands for three hosts:
         tc qdisc add dev eth0 root handle 1: hfsc
         tc class add dev eth0 parent 1: classid 1:99 hfsc sc rate 80mbit \
                 ul rate 80mbit
@@ -20,24 +23,22 @@ from nova.openstack.common import processutils
             match ip src 10.0.0.3 flowid 1:2
         tc filter add dev eth0 parent 1: protocol ip prio 1 u32 \
             match ip src 10.0.0.2 flowid 1:1
-    """
 
-# TODO check if qdiscs not already the same, if yes don't delete and set up again
-
-# TODO interface is hardcoded for now as resource_allocation method does not account for the specific configuration in use
-def hfsc_proportional_share(interface, prios, upper_limit):
-    """
-
-    :rtype: bool
+    :param interface: interface on which to set the qdisc
+    :param prios: instance priorities mapped to their ip
+    :param upper_limit: upper limit for network bandwidth
+    :return: boolean (false in case of error)
     """
 
     result = True
 
-    default_class = 99
+    #get default class
+    default_class = max(prios)
+
+    #calculate sum of all priorities
     prio_sum = 0
     for prio in prios:
         prio_sum += prio
-        default_class = 1
 
     # add root qdisc
     try:
@@ -47,7 +48,6 @@ def hfsc_proportional_share(interface, prios, upper_limit):
         result = False
 
     # add root class
-    # TODO add CONF.fairness.hfsc_rate
     try:
         utils.execute('tc', 'class', 'add', 'dev', interface,
                       'parent', '1:', 'classid', '1:99', 'hfsc', 'sc',
@@ -56,7 +56,6 @@ def hfsc_proportional_share(interface, prios, upper_limit):
     except processutils.ProcessExecutionError:
         result = False
 
-    """
     # create child classes
     for prio in sorted(prios):
         classid = '1:%s' % prio
@@ -80,64 +79,19 @@ def hfsc_proportional_share(interface, prios, upper_limit):
                               classid, run_as_root=True)
             except processutils.ProcessExecutionError:
                 result = False
-    """
-
-    try:
-        utils.execute('tc', 'class', 'add', 'dev', interface,
-                      'parent', '1:99', 'classid', '1:1', 'hfsc', 'ls',
-                      'rate', '50mbit', 'ul', 'rate', '100mbit',
-                      run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-    try:
-        utils.execute('tc', 'class', 'add', 'dev', interface,
-                      'parent', '1:99', 'classid', '1:2', 'hfsc', 'ls',
-                      'rate', '30mbit', 'ul', 'rate', '100mbit',
-                      run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-    try:
-        utils.execute('tc', 'class', 'add', 'dev', interface,
-                      'parent', '1:99', 'classid', '1:3', 'hfsc', 'ls',
-                      'rate', '10mbit', 'ul', 'rate', '100mbit',
-                      run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-    try:
-        utils.execute('tc', 'filter', 'add', 'dev', interface,
-                      'parent', '1:', 'protocol', 'ip', 'prio', '1',
-                      'u32', 'match', 'ip', 'src', '10.0.100.2', 'flowid',
-                      '1:1', run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-    try:
-        utils.execute('tc', 'filter', 'add', 'dev', interface,
-                      'parent', '1:', 'protocol', 'ip', 'prio', '1',
-                      'u32', 'match', 'ip', 'src', '10.0.100.3', 'flowid',
-                      '1:2', run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-    try:
-        utils.execute('tc', 'filter', 'add', 'dev', interface,
-                      'parent', '1:', 'protocol', 'ip', 'prio', '1',
-                      'u32', 'match', 'ip', 'src', '10.0.100.4', 'flowid',
-                      '1:3', run_as_root=True)
-    except processutils.ProcessExecutionError:
-        result = False
-
-
 
     return result
 
 def reset_qdisc(interface):
+    """
+    Resets the qdisc
+
+    :param interface: interface on which to reset qdisc
+    :return: false in case of error
+    """
+    
     try:
         utils.execute('tc', 'qdisc', 'del', 'dev', interface,
                       'root', run_as_root=True)
     except processutils.ProcessExecutionError:
         return False
-
